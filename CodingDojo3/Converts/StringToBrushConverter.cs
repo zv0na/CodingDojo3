6e02d87baaabe535f1603bfc6f587a0153a16673
Patrick Zvonarits <patrick.zvonarits@gmx.at>
2017-11-17 10:38:17 +0100

    end

--- CodingDojo3/Converts/StringToBrushConverter.cs
+++ CodingDojo3/Converts/StringToBrushConverter.cs
@@ -1,13 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
 
 namespace CodingDojo3.Converts
 {
-    class StringToBrushConverter
+    public class StringToBrushConverter : IValueConverter
     {
-        
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string temp = (string)value;
+            if (temp.Equals("Enabled"))
+            {
+                return new SolidColorBrush(Colors.Green);
+            }
+            else
+            {
+                return new SolidColorBrush(Colors.Red);
+
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
